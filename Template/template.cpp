96c856ef961441bdf543c2ef9986d777afd20710
sandeeppathry21 <sandeeppathry21@gmail.com>
2014-06-24 20:06:29 +0530

    Update template.cpp

--- Template/template.cpp
+++ Template/template.cpp
@@ -1,1 +1,27 @@
-#
+#include<iostream>
+#include<cstdio>
+#include<set>
+#include<map>
+#include<algorithm>
+#include<memory.h>
+#include<string>
+#include<cmath>
+#include<vector>
+#include<stack>
+#include<queue>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+#define LL long long
+#define inp(n) scanf("%d",&(n))
+#define out(n) printf("%d",(n))
+#define SP putchar(' ')
+#define NL putchar('\n')
+
+int main()
+{
+
+}
